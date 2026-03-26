9b57c21962672da5634724249e089bbb246133fc
Aman Shahi <amanshahidev@gmail.com>
2026-03-26 22:34:38 +0530

    configured cors and setup backend deployment

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
 require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
 const urlRoute = require("./routes/url");
 const { connectDB } = require("./config/dbConnection");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 connectDB(process.env.MONGO_URI);
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 app.use("/shorten", urlRoute);
